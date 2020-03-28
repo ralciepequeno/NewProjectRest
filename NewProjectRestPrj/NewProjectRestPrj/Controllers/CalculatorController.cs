3f76562f54954af152028559d3efada258cf44ad
Ralcie <ralcie@gmail.com>
2020-03-27 22:58:12 -0300

    Controller

---
+++ NewProjectRestPrj/NewProjectRestPrj/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewProjectRestPrj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Sum(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest ("Invalid Input");
+        }
+
+        private int ConvertToDecimal(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
