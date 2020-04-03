f6b262eee4cb0625f0d77dffb2ba545e3d659e65
Ralcie <ralcie@gmail.com>
2020-04-02 21:12:24 -0300

    camadas

---
+++ NewProjectRestPrj - SegundaVersaoSemCamada/NewProjectRestPrj/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using NewProjectRestPrj.Model;
+using System.Collections.Generic;
+
+namespace NewProjectRestPrj.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long Id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete (long Id);
+    }
+}
